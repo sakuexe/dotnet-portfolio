050bc41161c930deaad58487600061e2ab764775
sakuexe <saku.karttunen@gmail.com>
2024-04-16 09:19:22 +0300

    added the base for the dashboard

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace fullstack_portfolio.Controllers;
+
+public class DashboardController : Controller
+{
+    private readonly ILogger<DashboardController> _logger;
+
+    public DashboardController(ILogger<DashboardController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
