11887081759d841c031d4ced7fff19436c9859b7
sakuexe <saku.karttunen@gmail.com>
2024-04-19 19:59:28 +0300

    Switched the way that the dashboard lists the controllers

--- Models/ViewModels/DashboardViewModel.cs
+++ Models/ViewModels/DashboardViewModel.cs
@@ -1,8 +1,22 @@
-using fullstack_portfolio.Data;
+using System.Reflection;
+using fullstack_portfolio.Controllers;
 
 namespace fullstack_portfolio.Models.ViewModels;
 
 public class DashboardViewModel
 {
-    public List<string> Collections = MongoContext.GetCollectionNames();
+    public List<string> Collections { get; set; } = new();
+
+    public DashboardViewModel()
+    {
+        var baseClass = typeof(GenericController<>);
+        // fetch controllers that inherit from GenericController
+        Assembly asm = Assembly.GetExecutingAssembly();
+        var controllers = asm.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.BaseType != null)
+            .Where(t => t.BaseType!.IsGenericType && t.BaseType.GetGenericTypeDefinition() == baseClass)
+            .Select(t => t.BaseType!.GetGenericArguments()[0].Name)
+            .ToList();
+        Collections = controllers;
+    }
 }
